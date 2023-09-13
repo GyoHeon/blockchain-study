678f50c0be50631f005df020a6bc1b8de43478a8
Gyoheon <ghl12078@gmail.com>
2023-09-13 12:14:54 +0900

    Feat: Test solidity variables

---
+++ hardhat/contracts/Variable.sol
@@ -0,0 +1,18 @@
+
+// SPDX-License-Identifier: UNLICENSED
+pragma solidity ^0.8.9;
+
+contract Variable{
+  // base
+  uint private _defaultValue;
+  uint public defaultValue;
+  uint constant CONSTANT_VALUE = 1234;
+  uint immutable IMMUTABLE_VALUE;
+
+  constructor() {
+    IMMUTABLE_VALUE = 12345;
+  }
+
+  address public addressValue = address(1);
+  address public defaultAddressValue;
+}
